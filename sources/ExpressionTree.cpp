7a711671533b2a949b63cd826a32907b227b231d
MetalcoreLoli <deadpool1118@gmail.com>
2020-11-12 09:21:31 +0300

    Add ExpressionTree::Insert Method

--- sources/ExpressionTree.cpp
+++ sources/ExpressionTree.cpp
@@ -1,9 +1,40 @@
 #include "../headers/ExpressionTree.h"
+#include <stdexcept>
+
 ExpressionTree::ExpressionTree()
 {
+    m_root = nullptr;
 }
 
 ExpressionTree::~ExpressionTree()
 {
     delete m_root;
+}
+
+bool InsertHelper(Expression* link, const Token& token)
+{
+    if (link == nullptr)
+    {
+        link = new Expression(token.GetType(), token.GetValue());
+    }
+    else 
+    {
+        if (link->GetLeft() == nullptr) 
+        {
+            return InsertHelper(link->GetLeft(), token);
+        }
+        else if (link->GetRight() == nullptr)
+        {
+            return InsertHelper(link->GetRight(), token);
+        }
+    }
+    return true;
+}
+
+ExpressionTree& ExpressionTree::Insert(const Token& token)
+{
+    if (InsertHelper(m_root, token))
+        return static_cast<ExpressionTree&>(*this);
+    else 
+        throw std::runtime_error{"insert operation was failed"};
 }
